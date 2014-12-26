5b000a083fd9dc96617a16634ebf191fafb2d54a
Mani bhushan <manibhushan.cs@gmail.com>
2014-12-26 17:44:09 +0530

    Test methods for add, mul, div, >, <

--- chap1-Introduction/FractionClient.py
+++ chap1-Introduction/FractionClient.py
@@ -8,6 +8,10 @@ def readFraction():
     return Fraction(n, d)
 
 
+def testFractionMultiplication(x, y):
+    return x * y
+
+
 def testFractionAddition(x, y):
     ans = x + y
     return ans
@@ -15,6 +19,22 @@ def testFractionAddition(x, y):
 
 def testFractionEquality(x, y):
     return x == y
+
+
+def testFractionDivision(x, y):
+    return x / y
+
+
+def testFractionSubtraction(x, y):
+    return x - y
+
+
+def testFractionGreaterThan(x, y):
+    return x > y
+
+
+def testFractionLessThan(x, y):
+    return x < y
 
 
 def main():
@@ -26,6 +46,21 @@ def main():
     print ("sum of {} and {}: {}".format(x, y, ans))
     ans = testFractionEquality(x, y)
     print ("Are {} and {} equal? {}".format(x, y, ans))
+
+    ans = testFractionMultiplication(x, y)
+    print ("multiplication of {} and {} is: {}".format(x, y, ans))
+
+    ans = testFractionDivision(x, y)
+    print ("Division of {}  and {} is: {}".format(x, y, ans))
+
+    ans = testFractionSubtraction(x, y)
+    print ("Subtract {} and {} equal to: {}".format(x, y, ans))
+
+    ans = testFractionGreaterThan(x, y)
+    print ("Is {} greater than {}: {}".format(x, y, ans))
+
+    ans = testFractionLessThan(x, y)
+    print ("Is {} less than {}: {}".format(x, y, ans))
 
 
 if __name__ == '__main__':

53e428a5ac3fee197b5f2f8c4c524088d1a59e81
Mani bhushan <manibhushan.cs@gmail.com>
2014-12-30 07:45:27 +0530

    War game of cards - added more apis - still need to fine tune it

--- chap1-Introduction/WarCardGame.py
+++ chap1-Introduction/WarCardGame.py
@@ -17,30 +17,142 @@ def playwar():
     print(compHand)
     print("Cards in player hands: ")
     print(playerHand)
-    play(playerHand, compHand)
+    nowPlay(playerHand, compHand)
 
 
-def play(player, computer):
+def computerPlay(compDeck, n=1):
+    key = input("Computer turn, any key for play: <n> to abort game:")
+    if key == 'n':
+        return None
+    cards = []
+    if compDeck.size() < n:
+        return cards
+    for i in range(n):
+        cards.append(compDeck.popCard())
+    return cards
 
-    while True:
-        battleCards = []
-        compCard = computer.popCard()
-        print ("Computer played: ", str(compCard))
-        key = input ("press any key to play your turn, <n> key to abort: ")
-        if key == 'n':
-            return
-        playerCard = player.popCard()
-        print ("You played: ", str(playerCard))
-        result = compCard.getRank() > playerCard.getRank()
-        if compCard.getRank() > playerCard.getRank():
-            print ("COMPUTER WON THIS HAND!!!")
-            computer.addMultipleCards([compCard, playerCard])
-        elif compCard.getRank() < playerCard.getRank():
-            print ("Congrats! YOU WON THIS HAND!!!")
-            player.addMultipleCards([compCard, playerCard])
+
+def playerPlay(playerDeck, n=1):
+    key = input("Player turn, any key for play: <n> to abort game:")
+    if key == 'n':
+        return None
+    cards = []
+    if playerDeck.size() < n:
+        return cards
+    for i in range(n):
+        cards.append(playerDeck.popCard())
+    return cards
+
+
+def compareCards(computer, player):
+    """ Assumes computer and player are list of cards
+    here we compare the topmost card of each computer and player
+    Return:
+        1 - if computer wins the hand
+        2 - if player wins the hand
+        3 - Tie
+        4 - computer wins the game.
+        5 - player wins the game.
+        if one of them has less card left than the other then they loose and
+        other wins the game.
+    """
+    clen = len(computer)
+    plen = len(player)
+    if clen == plen:
+        if computer[clen-1].getRank() > player[plen-1].getRank():
+            return 1
+        elif computer[clen-1].getRank() < player[plen-1].getRank():
+            return 2
         else:
-            print ("Equal Cards - Done with game!")
+            return 3
+    elif clen > plen:
+        return 4
+    else:
+        return 5
+
+
+def showPlayedCards(cards, subject):
+    print (subject + " played: ")
+    for card in cards:
+        print (str(card))
+
+
+def nowPlay(player, computer):
+    while True:
+        tableCards = []
+        compCards = computerPlay(computer)
+        if compCards is None:
+            print ("You terminated the Game!")
             return
+        elif len(compCards) == 0:
+            print ("YOU WON!! COngratulations :)")
+            return
+        tableCards.extend(compCards)
+        showPlayedCards(compCards, "Computer")
+
+        playerCards = playerPlay(player)
+
+        if playerCards is None:
+            print ("You terminated the Game!")
+            return
+        elif len(playerCards) == 0:
+            print ("Computer Won!! Congrats to her :)")
+            return
+        tableCards.extend(playerCards)
+        showPlayedCards(playerCards, "You")
+
+        result = compareCards(compCards, playerCards)
+        if result == 1:
+            computer.addMultipleCards(tableCards)
+        elif result == 2:
+            player.addMultipleCards(tableCards)
+        elif result == 4:
+            print ("Computer Won!! Congrats to her :)")
+            return
+        elif result == 5:
+            print ("YOU WON!! COngratulations :)")
+            return
+        elif result == 3:
+            while True:
+                print ("EQUAL Cards - Now each player will put 2 cards on table\
+                   - One face down and one face up. Player having high face up\
+                   card would win the hand and take all the cards on the \
+                   table.")
+
+                compCards = computerPlay(computer)
+                if compCards is None:
+                    print ("You terminated the Game!")
+                    return
+                elif len(compCards) == 0:
+                    print ("YOU WON!! COngratulations :)")
+                    return
+                tableCards.extend(compCards)
+                showPlayedCards(compCards, "Computer")
+
+                playerCards = playerPlay(player)
+                if playerCards is None:
+                    print ("You terminated the Game!")
+                    return
+                elif len(playerCards) == 0:
+                    print ("Computer Won!! Congrats to her :)")
+                    return
+                showPlayedCards(playerCards, "You")
+                tableCards.extend(playerCards)
+
+                result = compareCards(compCards, playerCards)
+
+                if result == 1:
+                    computer.addMultipleCards(tableCards)
+                    break
+                elif result == 2:
+                    player.addMultipleCards(tableCards)
+                    break
+                elif result == 4:
+                    print ("Computer Won!! Congrats to her :)")
+                    return
+                elif result == 5:
+                    print ("YOU WON!! COngratulations :)")
+                    return
 
 
 def main():
@@ -49,4 +161,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-

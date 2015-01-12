8ef68d2b937c58aebe3236e86d12e93d5429557a
Mani bhushan <manibhushan.cs@gmail.com>
2015-01-12 07:22:25 +0530

    Printer Task class

---
+++ chap3-BasicDataStructure/printingtask/Task.py
@@ -0,0 +1,17 @@
+import random
+
+
+class Task:
+
+    def __init__(self, time):
+        self.timeStamp = time
+        self.pages = random.randrange(1, 21)
+
+    def getStamp(self):
+        return self.timeStamp
+
+    def getPages(self):
+        return self.pages
+
+    def waitTime(self, currentTime):
+        return currentTime - self.timeStamp

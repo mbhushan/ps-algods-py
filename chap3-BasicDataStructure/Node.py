aa6b5c37c183a01bb36fd1284e1aa8644e198458
Mani bhushan <manibhushan.cs@gmail.com>
2015-01-13 06:10:19 +0530

    Node class for linked list in python

---
+++ chap3-BasicDataStructure/Node.py
@@ -0,0 +1,18 @@
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def setData(self, data):
+        self.data = data
+
+    def getNext(self):
+        return self.next
+
+    def setNext(self, next):
+        self.next = next

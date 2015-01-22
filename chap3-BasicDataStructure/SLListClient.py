01647a75ec1b54f6d4b7a61ec42004e147dde539
Mani bhushan <manibhushan.cs@gmail.com>
2015-01-22 06:35:47 +0530

    test methods for pop, append, insert, search and index

--- chap3-BasicDataStructure/SLListClient.py
+++ chap3-BasicDataStructure/SLListClient.py
@@ -9,8 +9,9 @@ def testLinkedList():
                "2 - Index\n",
                "3 - Pop\n",
                "4 - Search\n",
-               "5 - Size\n",
-               "6 - Exit\n")
+               "5 - Insert\n",
+               "6 - Size\n",
+               "7 - Exit\n")
         choice = input()
         if choice == '1':
             ll = addll(ll)
@@ -20,29 +21,52 @@ def testLinkedList():
             ll.__str__()
         elif choice == '3':
             ll = pop(ll)
-            # searchKey(ll)
             ll.__str__()
         elif choice == '4':
-            ll = pop(ll)
-            # searchKey(ll)
+            searchKey(ll)
             ll.__str__()
         elif choice == '5':
-            size(ll)
+            insert(ll)
             ll.__str__()
         elif choice == '6':
+            size(ll)
+            ll.__str__()
+        elif choice == '7':
             break
         else:
             print ("BAD Choice! Choose from 1 to 4 numbers")
 
 
+def insert(ll):
+    print ("Enter data to be inserted: ")
+    data = input()
+    print ("Index at which data to be inserted: ")
+    index = input()
+    index = index.strip()
+    if not index.isdigit():
+        print ("Bad Index!")
+    index = int(index)
+    if index < 0 or index > ll.size():
+        print ("Index out of range!")
+    ll.insert(data, index)
+    return ll
+
+
 def pop(ll):
     print ("Enter index to be popped: ")
     index = input()
+    index = index.strip()
+    try:
+        index = int(index)
+    except:
+        print ("Bad Input for Index")
+        return ll
     data = ll.pop(index)
     if data is None:
         print ("No data found for index: ", index)
     else:
         print("Popped: ", data)
+    return ll
 
 
 def index(ll):
@@ -62,22 +86,12 @@ def addll(ll):
     return ll
 
 
-def removell(ll):
-    print ("Data to remove: ")
-    data = input()
-    result = ll.remove(data)
-    if result:
-        print("REMOVED: ", data)
-    else:
-        print("Not Found in linked list!")
-
-
 def searchKey(ll):
     print ("Enter data to be searched: ")
     data = input()
     result = ll.search(data)
-    if result:
-        print ("FOUND!")
+    if result >= 0:
+        print ("FOUND at index: ", result)
     else:
         print ("Not Found!")
 

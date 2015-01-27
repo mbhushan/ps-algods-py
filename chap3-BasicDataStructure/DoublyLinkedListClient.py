49edb1d8e68d4ed022898261d214d2834e447bb0
Mani bhushan <manibhushan.cs@gmail.com>
2015-01-27 07:19:27 +0530

    Added Client Test code for removeFront, removeRear, removeIndex and search

--- chap3-BasicDataStructure/DoublyLinkedListClient.py
+++ chap3-BasicDataStructure/DoublyLinkedListClient.py
@@ -11,18 +11,30 @@ def testDoublyLinkedList():
               "4 - removeFront()\n",
               "5 - removeRear()\n",
               "6 - remove(index)\n",
-              "7 - index()\n",
+              "7 - search(key)\n",
               "8 - size()\n",
               "9 - exit()")
         choice = input("Enter Choice: ")
         if choice == '1':
             dll = addFront(dll)
             dll.__str__()
-        if choice == '2':
+        elif choice == '2':
             dll = addRear(dll)
             dll.__str__()
-        if choice == '3':
+        elif choice == '3':
             dll = insertDLL(dll)
+            dll.__str__()
+        elif choice == '4':
+            dll = removeFront(dll)
+            dll.__str__()
+        elif choice == '5':
+            dll = removeRear(dll)
+            dll.__str__()
+        elif choice == '6':
+            dll = removeIndex(dll)
+            dll.__str__()
+        elif choice == '7':
+            search(dll)
             dll.__str__()
         elif choice == '8':
             dll.__str__()
@@ -32,6 +44,48 @@ def testDoublyLinkedList():
         else:
             print ("Bad Choice. Choose valid operation!")
             continue
+
+
+def search(dll):
+    key = input("Enter search key: ")
+    index = dll.search(key)
+    if index >= 0:
+        print ("Key: %s -> found at index: %d" % (key, index))
+    else:
+        print ("Key: %s -> Not Found in list" % key)
+
+
+def removeIndex(dll):
+    index = input("Enter index: ")
+    index = index.strip()
+    if not index.isdigit():
+        print ("Bad Index Value")
+        return dll
+    index = int(index)
+    node = dll.removeIndex(index)
+    if node:
+        print ("Removed: ", node.data)
+    else:
+        print ("Remove Failed!")
+    return dll
+
+
+def removeRear(dll):
+    node = dll.removeRear()
+    if node:
+        print ("Removed: ", node.data)
+    else:
+        print ("Rear remove failed!")
+    return dll
+
+
+def removeFront(dll):
+    node = dll.removeFront()
+    if node:
+        print ("Removed: ", node.data)
+    else:
+        print ("Front Remove Failed!")
+    return dll
 
 
 def insertDLL(dll):
